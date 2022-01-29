1dc52c0a7375d4f0ae5b727e12c0cc3da8fac6f5
Jeremy Banks <_@jeremy.ca>
2022-01-29 21:48:29 +0000

    r8/1dc9/521d

---
+++ examples/excessive.rs
@@ -0,0 +1,82 @@
+use {
+    indexmap::IndexMap,
+    std::{
+        collections::{BTreeMap, HashMap},
+        fmt::Debug,
+    },
+};
+
+// #[ez::main]
+// fn main(args: &[&str], env: &OrderedMap<String, String>) {
+
+// }
+
+// #[ez::main]
+// async fn main_2(args: {
+//     /// Turns the thing on.
+//     flag: boolean = false,
+// }, env: &OrderedMap<String, String>) -> u8 {
+
+// }
+
+trait Main: Fn(Self::Args, Self::Env) -> Self::ExitStatus {
+    type Args: MainArgs;
+    type Env: MainEnv;
+    type ExitStatus: MainExitStatus;
+}
+
+impl<Args, Env, ExitStatus> Main for fn(Args, Env) -> ExitStatus
+where
+    Args: MainArgs,
+    Env: MainEnv,
+    ExitStatus: MainExitStatus,
+{
+    type Args = Args;
+    type Env = Env;
+    type ExitStatus = ExitStatus;
+}
+
+trait MainArgs: FromIterator<String> {}
+impl<T> MainArgs for T where T: FromIterator<String> {}
+
+trait MainEnv: FromIterator<(String, String)> {}
+impl<T> MainEnv for T where T: FromIterator<(String, String)> {}
+
+trait MainExitStatus: Into<i32> {}
+impl<T> MainExitStatus for T where T: Into<i32> {}
+
+pub fn main() {
+    fn run_main<F: Main>(f: F) {
+        // TODO: don't panic on UTF-8 errors
+        let args = std::env::args().skip(1).map(|s| s.to_string());
+        let args = F::Args::from_iter(args);
+
+        // TODO: don't panic on UTF-8 errors
+        let env = std::env::vars().map(|(a, b)| (a.to_string(), b.to_string()));
+        let env = F::Env::from_iter(env);
+
+        let status = f(args, env).into();
+    }
+
+    let i = vec![
+        "foo".to_string(),
+        "bar".to_string(),
+        "baz".to_string(),
+        "qux".to_string(),
+    ]
+    .into_iter();
+    Vec::<String>::from_iter(i);
+
+    let i = vec![
+        ("foo".to_string(), "bar".to_string()),
+        ("baz".to_string(), "qux".to_string()),
+    ]
+    .into_iter();
+    HashMap::<String, String>::from_iter(i);
+
+    fn inner_main(args: Vec<String>, env: HashMap<String, String>) -> i32 {
+        return 0;
+    }
+
+    run_main(inner_main as fn(_, _) -> _);
+}
--- src/main.md
+++ src/main.md
@@ -9,19 +9,15 @@ It has the following effects:
   - we default to log level `WARN` for imported crates
   - we default to log level `INFO` for the current crate
 - wraps main in a `Result` with an `eyre::Report` to enable use of `?` to
-  propogate top-level errors into a crash.
+  propagate top-level errors into a crash.
 - `main()` can optionally be defined with primitive integer return type (or an
   `ez::Int`). if so, the process will use `main()`'s return value as its exit
   status, as in C. (This ensures that all destructors on the stack have had a
   chance to run, which is not the case when calling `std::process::exit()`
   directly.)
-- `main()` can optionally be defined to take one or two arguments, which will be
-  the values and count (respectively) of the arguments the process was invoked
-  with, like `argv` and `argc` in C.
-  - `argv` must be a `Vec<String>`. If any of the arguments are not valid UTF-8,
+- `main()` can optionally be defined to take one or two arguments:
+  - `args` must be a `&[&str]`. If any of the arguments are not valid UTF-8,
     a warning will be logged and invalid segments will be replaced with the
     replacement character `'�'`.
-  - `argc` must be a `usize` or an `ez::Int`
-  - `envp` must be a `Map<String, String>`. If any keys or values are not valid
-    UTF-8, an error will be logged and they will be excluded from the list. This
-    is a frozen copy and does not reflect any changes later.
+  - `env` must be a `&HashMap<&str, &str>`. If any keys or values are not valid
+    UTF-8, an warning will be logged and they will be excluded from the list.
