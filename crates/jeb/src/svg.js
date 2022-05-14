2f616e852e900757589b050e3a02592c2cdb8c9d
Jeremy Banks <_@jeremy.ca>
2022-05-14 16:04:34 +0000

    r247/2f67/0d17

--- crates/jeb/src/svg.js
+++
@@ -1,7 +0,0 @@
-document.documentElement.classList.add("interactive");
-for (const def of document.querySelectorAll("defs.interactive")) {
-    for (const child of def.children) {
-        def.before(child);
-    }
-    def.remove();
-}
