90aea29a23952cb152a4f36ef7cdb00482094860
Jeremy Banks <_@jeremy.ca>
2022-05-19 16:39:44 +0000

    r282/90ac/c6e6

---
+++ crates/doop/tests/compile_fail/trailing_commas.rs
@@ -0,0 +1,19 @@
+fn main() {
+    doop::doop!{
+        let Numbers = [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+        ];
+        static {
+            let n = 0;
+        }
+        for N in Numbers {
+            let n = n + N;
+        }
+    }
+    assert_eq!(n, 21);
+}
