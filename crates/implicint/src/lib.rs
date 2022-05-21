317547b713c489892a1d5d911a7b9af8fa1a4d07
Jeremy Banks <_@jeremy.ca>
2022-05-21 22:48:23 +0000

    r299/3175/f59d

--- crates/implicint/src/lib.rs
+++ crates/implicint/src/lib.rs
@@ -10,97 +10,93 @@ use ::{
     },
     doop::doop,
     thiserror::Error,
-    paste::paste,
 };
 
 // one thing at a fucking time, jesus christ.
 // first, basic ops, checked and panicking(!).
 // then add coercion from strictly compatible types.
-// then add .into() and .from() for all integer types, panicking or throwing as appropriate.
-// then add int() converting from approximately maybe compatible types.
-
-
-
-#[derive(Clone, Eq, Ord, PartialEq, PartialOrd, Default, Hash, Copy)]
-pub struct Int<M> {
-    value: i128,
-}
-
-#[derive(Error, Debug)]
-pub enum IntError {
-    #[error(transparent)]
-    AddOverflow(IntAddOverflowError),
-    #[error(transparent)]
-    SubOverflow(Int, Int),
-    #[error(transparent)]
-    MulOverflow(Int, Int),
-    #[error(transparent)]
-    DivisionByZero(Int),
-}
-
-#[derive(Error, Debug)]
-#[error("result overflowed when adding: `{.lhs} + {.rhs}`")]
-pub struct IntAddOverflowError {
-    pub lhs: Int,
-    pub rhs: Int,
-}
-
-#[derive(Error, Debug)]
-#[error("result overflowed when subtracting: `{.lhs} - {.rhs}`")]
-pub struct IntSubOverflowError {
-    pub lhs: Int,
-    pub rhs: Int,
-}
-
-#[derive(Error, Debug)]
-#[error("result overflowed when multiplying: `{.lhs} * {.rhs}`")]
-pub struct IntMulOverflowError {
-    pub lhs: Int,
-    pub rhs: Int,
-}
-
-#[derive(Error, Debug)]
-#[error("divided by zero: `{.lhs} / 0`")]
-pub struct IntDivisionByZeroError {
-    pub lhs: Int,
-}
-
-
-impl Int {
-    pub const MAX: Self = Self { value: i128::MAX };
-    pub const MIN: Self = Self { value: i128::MIN };
-}
-
-pub(crate) trait IntImpl: Into<Int> {
-    pub fn try_add(self, rhs: Int) -> Result<Int, IntError> {
-        let lhs = self.into();
-        let rhs = rhs.into();
-        let result = lhs.value.checked_add(rhs.value);
-        if let Some(result) = result {
-            Ok(result.into())
-        } else {
-            Err(IntError::AddOverflow(lhs, rhs))
-        }
-    }
-}
-impl IntImpl for Int {}
-
-impl Add<Int<Checked>> for Int<Checked> {
-    type Output = Int<Checked>;
-
-    fn add(self, rhs: Int<Checked>) -> Self::Output {
-        let result = self.value.checked_add(rhs.value);
-        match result {
-            Some(value) => Ok(Int::new(value)),
-            None => Err(IntError::AddOverflow(self, rhs)),
-        }
-    }
-}
-
-pub trait ErrorMode {}
-type DefaultErrorMode = Panicking;
+// then add .into() and .from() for all integer types, panicking or throwing as
+// appropriate. then add int() converting from approximately maybe compatible
+// types.
 
 doop! {
+    static {
+        #[derive(Clone, Eq, Ord, PartialEq, PartialOrd, Default, Hash, Copy)]
+        pub struct Int<M> {
+            value: i128,
+        }
+
+        #[derive(Error, Debug)]
+        pub enum IntError {
+            AddOverflow(Int, Int),
+            SubOverflow(Int, Int),
+            MulOverflow(Int, Int),
+            DivisionByZero(Int),
+        }
+
+        #[derive(Error, Debug)]
+        #[error("result overflowed when adding: `{.lhs} + {.rhs}`")]
+        pub struct IntAddOverflowError {
+            pub lhs: Int,
+            pub rhs: Int,
+        }
+
+        #[derive(Error, Debug)]
+        #[error("result overflowed when subtracting: `{.lhs} - {.rhs}`")]
+        pub struct IntSubOverflowError {
+            pub lhs: Int,
+            pub rhs: Int,
+        }
+
+        #[derive(Error, Debug)]
+        #[error("result overflowed when multiplying: `{.lhs} * {.rhs}`")]
+        pub struct IntMulOverflowError {
+            pub lhs: Int,
+            pub rhs: Int,
+        }
+
+        #[derive(Error, Debug)]
+        #[error("divided by zero: `{.lhs} / 0`")]
+        pub struct IntDivisionByZeroError {
+            pub lhs: Int,
+        }
+
+
+        impl Int {
+            pub const MAX: Self = Self { value: i128::MAX };
+            pub const MIN: Self = Self { value: i128::MIN };
+        }
+
+        pub(crate) trait IntImpl: Into<Int> {
+            pub fn try_add(self, rhs: Int) -> Result<Int, IntError> {
+                let lhs = self.into();
+                let rhs = rhs.into();
+                let result = lhs.value.checked_add(rhs.value);
+                if let Some(result) = result {
+                    Ok(result.into())
+                } else {
+                    Err(IntError::AddOverflow(lhs, rhs))
+                }
+            }
+        }
+        impl IntImpl for Int {}
+
+        impl Add<Int<Checked>> for Int<Checked> {
+            type Output = Int<Checked>;
+
+            fn add(self, rhs: Int<Checked>) -> Self::Output {
+                let result = self.value.checked_add(rhs.value);
+                match result {
+                    Some(value) => Ok(Int::new(value)),
+                    None => Err(IntError::AddOverflow(self, rhs)),
+                }
+            }
+        }
+
+        pub trait ErrorMode {}
+        type DefaultErrorMode = Panicking;
+    }
+
     let ERROR_MODE_LIST = [
         (Infer, unwrap()),
         (Panicking, unwrap()),
@@ -112,8 +108,6 @@ doop! {
         pub struct ERROR_MODE;
         impl ErrorMode for ERROR_MODE {}
     }
-
-
 }
 
 #[test]
