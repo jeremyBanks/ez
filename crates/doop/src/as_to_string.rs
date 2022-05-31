6e6d44bab60dd835aab39212bfa004c5552fbe52
Jeremy Banks <_@jeremy.ca>
2022-05-31 17:03:26 +0000

    r344/6e6c/8018

--- crates/doop/src/as_to_string.rs
+++ crates/doop/src/as_to_string.rs
@@ -8,31 +8,50 @@ use {
     },
 };
 
+// This should be AsRef<str> not CompareAsStr.
+
+// Two types:
+// One that adds an AsRef<str> and a Format<String> to a type, using a memoized to_string
+// on the underlying type.
+// This is non-zero-sized, of course.
+
+// One that takes an AsRef<str> type and uses it for all comparison operators
+// This is a zero-sized wrapper.
+
 pub fn main() {
-    let mut numbers = vec![20, 22, 23, 24, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-    println!("original {numbers:?}");
-    numbers.sort();
-    println!("sorted {numbers:?}");
-    let mut numbers: Vec<_> = numbers.iter().map(AsToString).collect();
-    numbers.sort();
-    println!("sorted as strings {numbers:?}");
-    let mut numbers: Vec<_> = numbers.into_iter().map(AsToString::unwrap).collect();
-    numbers.sort();
+    let mut numbers = Vec::from_iter(b"Hello, world!".into_iter().copied());
+
 }
 
-pub fn like_string<Inner: Display + Debug>(inner: Inner) -> AsToString<SavedToString<Inner>> {
-    AsToString::wrap(SavedToString::wrap(inner))
+/// Wrapper type implementing AsRef<str> and Display, memoizing the inner value's to_string().
+pub struct AsLazyStr<Inner: Display> {
+    inner: Inner,
+    string: OnceCell<String>,
 }
 
-pub fn unlike_string<Inner: Display + Debug>(wrapped: AsToString<SavedToString<Inner>>) -> Inner {
-    wrapped.unwrap().unwrap()
+impl<Inner: Display> AsLazyStr<Inner> {
+    pub fn wrap(inner: Inner) -> AsLazyStr<Inner> {
+        AsLazyStr { inner, string: OnceCell::new() }
+    }
+
+    pub fn unwrap(self) -> Inner {
+        self.inner
+    }
 }
 
-pub struct AsToString<Inner: Display + Debug>(Inner);
+impl<Inner: Display> AsRef<str> for AsLazyStr<Inner> {
+    fn as_ref(&self) -> &str {
+        self.string.get_or_init(|| self.inner.to_string())
+    }
+}
 
-impl<Inner: Display + Debug> AsToString<Inner> {
-    pub fn wrap(inner: Inner) -> AsToString<Inner> {
-        AsToString(inner)
+/// Wrapper newtype implementing comparison operators `Eq`, `Ord`, `PartialEq`, PartialOrd`, and `Hash`
+/// for the inner type based on the result of its `AsRef<str>`.
+pub struct CompareAsStr<Inner: AsRef<str>>(Inner);
+
+impl<Inner: AsRef<str>> CompareAsStr<Inner> {
+    pub fn wrap(inner: Inner) -> CompareAsStr<Inner> {
+        CompareAsStr(inner)
     }
 
     pub fn unwrap(self) -> Inner {
@@ -40,76 +59,57 @@ impl<Inner: Display + Debug> AsToString<Inner> {
     }
 }
 
-pub struct SavedToString<Inner: Display + Debug> {
-    inner: Inner,
-    string: OnceCell<String>,
-}
-
-impl<Inner: Display + Debug> SavedToString<Inner> {
-    pub fn wrap(inner: Inner) -> SavedToString<Inner> {
-        SavedToString { inner, string: OnceCell::new() }
-    }
-
-    pub fn unwrap(self) -> Inner {
-        self.inner
-    }
-
-    pub fn as_str(&self) -> &str {
-        self.string.get_or_init(|| self.inner.to_string()).as_str()
-    }
-}
-
-impl<Inner: Display + Debug> Display for SavedToString<Inner> {
+impl<Inner: Display> Display for AsLazyStr<Inner> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Display::fmt(self.as_str(), f)
+        Display::fmt(self.as_ref(), f)
     }
 }
 
-impl<Inner: Display + Debug> Debug for SavedToString<Inner> {
+impl<Inner: Display> Debug for AsLazyStr<Inner> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Debug::fmt(&self.inner, f)
     }
 }
 
-impl<Inner: Display + Debug> Display for AsToString<Inner> {
+impl<Inner: AsRef<str>> Display for CompareAsStr<Inner> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Display::fmt(&self.0, f)
     }
 }
 
-impl<Inner: Display + Debug> Debug for AsToString<Inner> {
+impl<Inner: AsRef<str>> Debug for CompareAsStr<Inner> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Debug::fmt(&self.0, f)
     }
 }
 
-impl<Inner: Display + Debug> Ord for AsToString<Inner> {
+impl<Inner: AsRef<str>> Ord for CompareAsStr<Inner> {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.to_string().cmp(&other.0.to_string())
+        self.as_ref().cmp(&other.0.as_ref())
     }
 }
 
-impl<Inner: Display + Debug> PartialOrd for AsToString<Inner> {
+impl<Inner: AsRef<str>> PartialOrd for CompareAsStr<Inner> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<Inner: Display + Debug> PartialEq for AsToString<Inner> {
+impl<Inner: AsRef<str>> PartialEq for CompareAsStr<Inner> {
     fn eq(&self, other: &Self) -> bool {
-        self.to_string() == other.0.to_string()
+        self.as_ref() == other.0.as_ref()
     }
 }
 
-impl<Inner: Display + Debug> Eq for AsToString<Inner> {}
+impl<Inner: AsRef<str>> Eq for CompareAsStr<Inner> {}
 
-impl<Inner: Display + Debug> Hash for AsToString<Inner> {
+impl<Inner: AsRef<str>> Hash for CompareAsStr<Inner> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.to_string().hash(state)
+        self.as_ref().hash(state)
     }
 }
 
-impl<Inner: Display + Debug> Deref for AsToString<Inner> {
+impl<Inner: AsRef<str>> Deref for CompareAsStr<Inner> {
     type Target = Inner;
 
     fn deref(&self) -> &Self::Target {
