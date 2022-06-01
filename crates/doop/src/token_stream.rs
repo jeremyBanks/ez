62f35af9012a7c5cedc39c071072d31f9c4f59f6
Jeremy Banks <_@jeremy.ca>
2022-06-01 22:42:48 +0000

    r350/62f4/9db2

--- crates/doop/src/token_stream.rs
+++ crates/doop/src/token_stream.rs
@@ -121,120 +121,120 @@ pub trait TokenStreamExt: Borrow<TokenStream> + BorrowMut<TokenStream> + Sized {
         }
     }
 
-    // fn flat_map_shallow<F, I>(&self, f: F) -> TokenStream
-    // where
-    //     F: Fn(TokenTree) -> I,
-    //     I: IntoIterator<Item = TokenTree>,
-    // {
-    //     self.to_iter().flat_map(f).collect()
-    // }
+    fn flat_map_shallow<F, I>(&self, f: F) -> TokenStream
+    where
+        F: Fn(TokenTree) -> I,
+        I: IntoIterator<Item = TokenTree>,
+    {
+        self.to_iter().flat_map(f).collect()
+    }
 
-    // fn flat_map_deep<F, I>(&self, f: F) -> TokenStream
-    // where
-    //     F: Fn(TokenTree) -> I,
-    //     I: IntoIterator<Item = TokenTree>,
-    // {
-    //     self.to_iter()
-    //         .flat_map(f)
-    //         .map(|tt| {
-    //             if let TokenTree::Group(group) = tt {
-    //                 let mut tt = TokenTree::Group(Group::new(
-    //                     group.delimiter(),
-    //                     group.stream().into_iter().flat_map(f).collect(),
-    //                 ));
-    //                 tt.set_span(group.span());
-    //                 tt
-    //             } else {
-    //                 tt
-    //             }
-    //         })
-    //         .collect()
-    // }
+    fn flat_map_deep<F, I>(&self, f: F) -> TokenStream
+    where
+        F: Fn(TokenTree) -> I,
+        I: IntoIterator<Item = TokenTree>,
+    {
+        self.to_iter()
+            .flat_map(f)
+            .map(|tt| {
+                if let TokenTree::Group(group) = tt {
+                    let mut tt = TokenTree::Group(Group::new(
+                        group.delimiter(),
+                        group.stream().into_iter().flat_map(f).collect(),
+                    ));
+                    tt.set_span(group.span());
+                    tt
+                } else {
+                    tt
+                }
+            })
+            .collect()
+    }
 
-    // fn with_span_shallow(&self, span: Span) -> TokenStream {
-    //     self.flat_map_shallow(|mut tree| {
-    //         tree.set_span(span);
-    //         Some(tree)
-    //     })
-    // }
+    fn with_span_shallow(&self, span: Span) -> TokenStream {
+        self.flat_map_shallow(|mut tree| {
+            tree.set_span(span);
+            Some(tree)
+        })
+    }
 
-    // fn with_span_deep(&self, span: Span) -> TokenStream {
-    //     self.flat_map_deep(|mut tree| {
-    //         tree.set_span(span);
-    //         Some(tree)
-    //     })
-    // }
+    fn with_span_deep(&self, span: Span) -> TokenStream {
+        self.flat_map_deep(|mut tree| {
+            tree.set_span(span);
+            Some(tree)
+        })
+    }
 
-    // fn replace_deep(&self, replacements: &HashMap<String, TokenStream>) ->
-    // TokenStream {     self.flat_map_deep(|tt| match tt {
-    //         TokenTree::Ident(ident) =>
-    //             if let Ok(replacement) = replacements.get(&ident.to_string()) {
-    //                 replacement.clone()
-    //             } else {
-    //                 TokenStream::from_iter([tt])
-    //             },
-    //         _ => TokenStream::from_iter([tt]),
-    //     })
-    // }
+    fn replace_deep(&self, replacements: &HashMap<String, TokenStream>) ->
+    TokenStream {     self.flat_map_deep(|tt| match tt {
+            TokenTree::Ident(ident) =>
+                if let Ok(replacement) = replacements.get(&ident.to_string()) {
+                    replacement.clone()
+                } else {
+                    TokenStream::from_iter([tt])
+                },
+            _ => TokenStream::from_iter([tt]),
+        })
+    }
 
-    // fn replace_shallow(&self, replacements: &HashMap<String, TokenStream>) ->
-    // TokenStream {     self.flat_map_shallow(|tt| match tt {
-    //         TokenTree::Ident(ident) =>
-    //             if let Ok(replacement) = replacements.get(&ident.to_string()) {
-    //                 replacement.clone()
-    //             } else {
-    //                 TokenStream::from_iter([tt])
-    //             },
-    //         _ => TokenStream::from_iter([tt]),
-    //     })
-    // }
+    fn replace_shallow(&self, replacements: &HashMap<String, TokenStream>) ->
+    TokenStream {     self.flat_map_shallow(|tt| match tt {
+            TokenTree::Ident(ident) =>
+                if let Ok(replacement) = replacements.get(&ident.to_string()) {
+                    replacement.clone()
+                } else {
+                    TokenStream::from_iter([tt])
+                },
+            _ => TokenStream::from_iter([tt]),
+        })
+    }
 
-    // fn split_lines(&self) -> TokenStreamIterator {
-    //     let vec = self.to_vec();
-    //     let mut lines = Vec::new();
-    //     let mut next_line_start_index = 0;
-    //     for (i, tt) in vec.iter().enumerate() {
-    //         match tt {
-    //             TokenTree::Punct(punct) if punct.as_char() == ';' => {
-    //                 lines.push(TokenStream::from_iter(
-    //                     vec[next_line_start_index..=i].iter().cloned(),
-    //                 ));
-    //                 next_line_start_index = i + 1;
-    //             }
-    //             TokenTree::Group(group) if group.delimiter() == Delimiter::Brace
-    // => {                 lines.push(TokenStream::from_iter(
-    //                     vec[next_line_start_index..=i].iter().cloned(),
-    //                 ));
-    //                 next_line_start_index = i + 1;
-    //             }
-    //             _ => {}
-    //         }
-    //     }
-    //     lines.push(TokenStream::from_iter(vec[next_line_start_index..].iter().
-    // cloned()));
+    fn split_lines(&self) -> TokenStreamIterator {
+        let vec = self.to_vec();
+        let mut lines = Vec::new();
+        let mut next_line_start_index = 0;
+        for (i, tt) in vec.iter().enumerate() {
+            match tt {
+                TokenTree::Punct(punct) if punct.as_char() == ';' => {
+                    lines.push(TokenStream::from_iter(
+                        vec[next_line_start_index..=i].iter().cloned(),
+                    ));
+                    next_line_start_index = i + 1;
+                }
+                TokenTree::Group(group) if group.delimiter() == Delimiter::Brace
+    => {                 lines.push(TokenStream::from_iter(
+                        vec[next_line_start_index..=i].iter().cloned(),
+                    ));
+                    next_line_start_index = i + 1;
+                }
+                _ => {}
+            }
+        }
+        lines.push(TokenStream::from_iter(vec[next_line_start_index..].iter().
+    cloned()));
 
-    //     lines.into_iter()
-    // }
+        lines.into_iter()
+    }
 
-    // fn split_commas(&self) -> TokenStreamIterator {
-    //     let vec = self.to_vec();
-    //     let mut slices = Vec::new();
-    //     let mut next_comma_start_index = 0;
-    //     for (i, tt) in vec.iter().enumerate() {
-    //         match tt {
-    //             TokenTree::Punct(punct) if punct.as_char() == ',' => {
-    //                 slices.push(TokenStream::from_iter(
-    //                     vec[next_comma_start_index..=i].iter().cloned(),
-    //                 ));
-    //                 next_comma_start_index = i + 1;
-    //             }
-    //             _ => {}
-    //         }
-    //     }
+    fn split_commas(&self) -> TokenStreamIterator {
+        let vec = self.to_vec();
+        let mut slices = Vec::new();
+        let mut next_comma_start_index = 0;
+        for (i, tt) in vec.iter().enumerate() {
+            match tt {
+                TokenTree::Punct(punct) if punct.as_char() == ',' => {
+                    slices.push(TokenStream::from_iter(
+                        vec[next_comma_start_index..=i].iter().cloned(),
+                    ));
+                    next_comma_start_index = i + 1;
+                }
+                _ => {}
+            }
+        }
 
-    //     slices.push(TokenStream::from_iter(vec[next_comma_start_index..].iter().
-    // cloned()));
+        slices.push(TokenStream::from_iter(vec[next_comma_start_index..].iter().
+    cloned()));
 
-    //     slices.into_iter()
-    // }
+        slices.into_iter()
+    }
 }
