c2cc7036f7a4e5199efc4073db3ba34a27f45ab8
Jeremy Banks <_@jeremy.ca>
2022-06-02 01:01:59 +0000

    r352/c2cd/79fe

--- crates/save/examples/generation.rs
+++ crates/save/examples/generation.rs
@@ -3,11 +3,19 @@ use {eyre::Result, git2::Repository, save::git2::CommitExt};
 fn main() -> Result<()> {
     let repo = Repository::open_from_env()?;
     let head = repo.head()?.peel_to_commit()?;
-    let generation_number = head.generation_number();
-    println!(
-        "The generation number of HEAD ({}) in the current repository is: {}",
-        &head.id().to_string()[..4],
-        generation_number
-    );
+    let tree = head.tree()?;
+    let head = &head.id().to_string()[..4];
+    let tree = &tree.id().to_string()[..4];
+
+    let first_parent_depth = 409;
+    let generation_index = 1647;
+    let commit_index = 1862;
+
+    println!("
+        HEAD is at commit {head} containing tree {tree}
+
+        r{first_parent_depth} / g{generation_index} / n{commit_index} / t{tree}
+    ");
+
     Ok(())
 }
