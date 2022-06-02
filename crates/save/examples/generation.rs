d6a29e032784b9f3efc533c1d35e3ff2301d55c1
Jeremy Banks <_@jeremy.ca>
2022-06-02 01:15:16 +0000

    r353/d6a1/c2cc

--- crates/save/examples/generation.rs
+++ crates/save/examples/generation.rs
@@ -4,17 +4,16 @@ fn main() -> Result<()> {
     let repo = Repository::open_from_env()?;
     let head = repo.head()?.peel_to_commit()?;
     let tree = head.tree()?;
-    let head = &head.id().to_string()[..4];
+    let head = &head.id().to_string()[..8];
     let tree = &tree.id().to_string()[..4];
 
-    let first_parent_depth = 409;
-    let generation_index = 1647;
-    let commit_index = 1862;
+    let revision = 409;
+    let generation = 1647;
+    let number = 1862;
 
     println!("
-        HEAD is at commit {head} containing tree {tree}
-
-        r{first_parent_depth} / g{generation_index} / n{commit_index} / t{tree}
+               id: {head}
+          message: {tree} at r{revision} / g{generation} / n{number}
     ");
 
     Ok(())
